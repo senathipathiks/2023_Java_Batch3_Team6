eb295099aae28b072bcf5c5a9103ecdf85b8dfae
Vinolisha <vinolisha8@gmail.com>
2023-12-16 14:38:31 +0530

    Vinolisha Changes Commited

--- MyClass.java
+++ MyClass.java
@@ -11,4 +11,12 @@ void surya(String x, String y)  //concat two string
   {
     System.out.println("Surya added this method");
   }
+void sub(int x, int y)  //adding two integers
+  {
+    System.out.println("Sub of two numbers: " + (x-y));
+  }
+void multi(int x, int y)  //adding two integers
+  {
+    System.out.println("Multiple of two numbers: " + (x*y));
+  }
 }
